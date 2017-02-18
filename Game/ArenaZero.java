bcfb3cdfd9221781866049f4fd1e24f3d3257140
purplxholic <zanette_cheng@mymail.sutd.edu.sg>
2017-02-18 13:00:40 +0800

    Pseudo code of game

---
+++ Game/ArenaZero.java
@@ -0,0 +1,119 @@
+package Week4;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Random;
+
+public class ArenaZero {
+	private int ID =0;
+	private String userName;
+	private Hashtable<String,Integer> coordinates; 
+	public int playerHealth=0; 
+	public String currentWeapon; 
+	private ArrayList<Weapon> weapons; 
+	public ArenaZero(){
+		System.out.println("You ID will be" + ID);
+		
+		ID++;
+		playerHealth = 100; 
+		initialiseWeapon();
+	}
+	
+	public ArenaZero(String name){
+		this.userName = name;
+		System.out.println("Welcome player " + userName);
+		coordinates = new Hashtable<String, Integer>();
+		coordinates.put("X", 0);
+		coordinates.put("Y", 0);
+		playerHealth = 100; 
+		initialiseWeapon();
+		
+	}
+
+
+	public void playerMovement(String direction){
+		if (direction.equals("left")){
+			int oldX = coordinates.get("X");
+			coordinates.put("X", new Integer(oldX-5));
+		}
+		else if (direction.equals("right")){
+			int oldX = coordinates.get("X");
+			coordinates.put("X", new Integer(oldX+5));
+
+		}
+		else if (direction.equals("up")){
+			int oldY = coordinates.get("Y");
+			coordinates.put("Y", new Integer(oldY+5));
+
+		}
+		else if (direction.equals("down")){
+			int oldY = coordinates.get("Y");
+			coordinates.put("Y", new Integer(oldY-5));
+		}
+		else{
+			System.out.println("Input error!");
+		}
+		for (String string:coordinates.keySet()){
+			System.out.println(string + " position: "+coordinates.get(string));
+		}
+
+	}
+	
+	public void initialiseWeapon(){
+		weapons = new ArrayList<>();
+		Sword sword = new Sword();
+		Gun gun = new Gun();
+		weapons.add(sword);
+		weapons.add(gun); 
+	}
+	public void getWeapon(){
+		System.out.println("A weapon box has spawned! It is a...\n");
+		Random random = new Random(); 
+		int index = random.nextInt(weapons.size());
+		System.out.println(weapons.get(index));
+		setPlayerHealth(weapons.get(index).setDamage());
+		
+		
+	}
+	
+	private void setPlayerHealth(int damage){
+		this.playerHealth += damage;
+		getplayerHealth(); //to update the player on his health
+	}
+
+	public void getplayerHealth(){
+		System.out.println("Your current health is: " + playerHealth);
+		
+	}
+}
+
+interface Weapon{
+	public String weaponName="";
+	public int damage=0; 
+	public String getName();
+	public int setDamage(); 
+}
+
+class Sword implements Weapon{
+	public String weaponName="Sword";
+	public int damage = -10; 
+	public String getName(){
+		return weaponName; 
+	}
+	public int setDamage(){
+		return damage; 
+	}
+	
+}
+class Gun implements Weapon{
+	public String weaponName="Gun";
+	public int damage = -30; 
+	public String getName(){
+		return weaponName; 
+		
+	}
+	public int setDamage(){
+		return damage;
+	}
+	
+}
---
+++ Game/ArenaZeroTest.java
@@ -0,0 +1,11 @@
+package Week4;
+
+public class ArenaZeroTest {
+	public static void main(String args[]){
+		ArenaZero paul = new ArenaZero("Paul");
+		
+		paul.playerMovement("up");
+		paul.getplayerHealth();
+		
+	}
+}
